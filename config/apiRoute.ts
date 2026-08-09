5c49928c876d0a77b6558916ab645481695b3a66
chi.suz <yochi.suz@gmail.com>
2026-08-09 23:01:13 +0700

    chore: add initial project structure

---
+++ config/apiRoute.ts
@@ -0,0 +1,12 @@
+const apiRoute = {
+  auth: {
+    login: "",
+    forgotPassword: "auth/forgot-password",
+    resetPasswordByCode: "",
+  },
+  registerProperty: {
+    registerUser: "",
+  },
+};
+
+export default apiRoute;
---
+++ config/clientRoute.ts
@@ -0,0 +1,13 @@
+export const rootPath = {
+  propertyContent: "",
+  rateAndAvailability: "",
+};
+
+const clientRoute = {
+  dashboard: "dashboard",
+  auth: {
+    login: "/auth/login",
+  },
+};
+
+export default clientRoute;
